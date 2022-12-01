ffe7fb10cbf3b2d25fa50cdd7afec9f61a608000
Peenaksh <85746966+Peenaksh@users.noreply.github.com>
2022-12-01 17:21:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ right_wrist_x = 0
 right_wrist_y = 0
 left_wrist_x = 0
 left_wrist_y = 0
-score_right_wrist = 0
+score_right_wrist = 
 score_left_wrist = 0
 
 function preload() {
@@ -28,6 +28,16 @@ function setup() {
 function gotPoses(results) {
 if (results.length > 0 ) {
     console.log(results)
+    score_right_wrist = results[0].pose.keypoints[10].score 
+    score_left_wrist = results[0].pose.keypoints[9].score 
+    console.log("scoreright wrist" + score_right_wrist + "score left wrist" + score_left_wrist);
+    right_wrist_x = results[0].pose.rightwrist.x
+    right_wrist_y = results[0].pose.rightwrist.y 
+    console.log("right wrist x= " + right_wrist_x + "right wrist y= " + right_wrist_y)
+
+    left_wrist_x = results[0].pose.leftwrist.x
+    left_wrist_y = results[0].pose.leftwrist.y 
+    console.log("left wrist x= " + left_wrist_x + "left wrist y= " + left_wrist_y)
 }
 }
 
